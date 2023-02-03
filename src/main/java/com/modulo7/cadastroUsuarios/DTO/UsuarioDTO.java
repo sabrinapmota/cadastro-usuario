5b1af14e76027e2434d6ab5013378e64910ec928
sabrinaporto <sabrinapmota@yahoo.com.br>
2023-02-03 14:20:13 -0300

    correções no dto

--- src/main/java/com/modulo7/cadastroUsuarios/DTO/UsuarioDTO.java
+++ src/main/java/com/modulo7/cadastroUsuarios/DTO/UsuarioDTO.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
+
 import javax.validation.constraints.NotNull;
 
 @Getter
@@ -14,23 +15,14 @@ public class UsuarioDTO {
     @NotBlank(message = "Nome do usuário deve ser preenchido.")
     @Length(min = 4, max = 20, message = "Nome do usuário deve conter de {min} a {max} caracteres.")
     private String nome;
-
-    @NotNull(message = "Idade deve ser preenchida.")
-    private int idade;
-
-    @NotBlank(message = "Telefone deve ser preenchido.")
-    @Length(min = 8, max = 9, message = "Telefone deve conter de {min} a {max} caracteres.")
-    private String telefone;
-
     @NotBlank(message = "Login deve ser preenchido.")
     @Length(min = 4, max = 12, message = "Login deve conter de {min} a {max} caracteres.")
     private String login;
-
     @NotBlank(message = "Senha deve ser preenchida.")
     @Length(min = 8, max = 20, message = "Senha deve conter de {min} a {max} caracteres.")
     private String senha;
 
     public UsuarioModel converterParaObjeto() {
-        return new UsuarioModel(nome, idade, telefone, login, senha);
+        return new UsuarioModel(nome, login, senha);
     }
 }
