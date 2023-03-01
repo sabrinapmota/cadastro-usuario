c65996f5c6c5af0fca2f5dc56998ba04a6ca9389
sabrinaporto <sabrinapmota@yahoo.com.br>
2023-03-01 12:32:02 -0300

    Teste de alterar usuário e buscar por id adicionados

--- src/test/java/com/modulo7/cadastroUsuarios/service/UsuarioServiceTest.java
+++ src/test/java/com/modulo7/cadastroUsuarios/service/UsuarioServiceTest.java
@@ -13,6 +13,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.internal.matchers.Any;
 
 import java.util.*;
 
@@ -63,6 +64,48 @@ class UsuarioServiceTest {
                 .thenReturn(usuarioModel);
 
         Assertions.assertNotEquals(usuarioModel.getSenha(), service.cadastrar(usuarioModel).getSenha());
+    }
+
+    @Test
+    public void deveriaBuscarPorId() {
+        UsuarioModel usuario = new UsuarioModel();
+        usuario.setId(1L);
+        usuario.setNome("Antonio");
+        usuario.setLogin("antonio");
+        usuario.setSenha("12345678");
+
+        Mockito.when(repository.findById(1L))
+                .thenReturn(Optional.of(usuario));
+
+        Optional<UsuarioModel> resposta = service.ttbuscarPorId(1L);
+
+        Assertions.assertEquals(usuario.getNome(), resposta.get().getNome());
+    }
+
+    @Test
+    public void deveriaAtualizarOUsuario() {
+        UsuarioModel usuarioAntigo = new UsuarioModel();
+        usuarioAntigo.setId(1L);
+        usuarioAntigo.setNome("Sabrina");
+        usuarioAntigo.setLogin("sabrina");
+        usuarioAntigo.setSenha("12345678");
+
+        Mockito.when(repository.findById(1L))
+                .thenReturn(Optional.of(usuarioAntigo));
+
+        UsuarioModel usuarioRespondido = new UsuarioModel();
+        usuarioRespondido.setId(1L);
+        usuarioRespondido.setNome("Sabrina Porto");
+        usuarioRespondido.setLogin("sabrinap");
+        usuarioRespondido.setSenha("12345679");
+
+        Mockito.when(repository.save(Any()))
+                .thenReturn(usuarioRespondido);
+
+        UsuarioDTO usuarioEnviado = new UsuarioDTO()
+
+
+        UsuarioModel usuarioFinal = service.alterar()
     }
 
 }
